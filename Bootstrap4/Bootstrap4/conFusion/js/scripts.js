5c3d718fbbafe2c8d10be84b39589c03a9952565
Dong <dongntde160034@fpt.edu.vn>
2023-06-06 23:49:49 +0700

    Lab4

--- Bootstrap4/Bootstrap4/conFusion/js/scripts.js
+++ Bootstrap4/Bootstrap4/conFusion/js/scripts.js
@@ -1,11 +1,33 @@
-$(document).ready(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carousel-pause").click(function(){
-        $("#mycarousel").carousel('pause');
-    });
-    $("#carousel-play").click(function(){
-        $("#mycarousel").carousel('cycle');
-    });
+var reserveTableButton = document.getElementById("reserveTableButton");
+reserveTableButton.addEventListener("click", function () {
+  $('#reservationModal').modal('toggle');
+});
 
+var loginLink = document.getElementById("loginLink");
+loginLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  $('#loginModal').modal('toggle');
+});
+$(document).ready(function () {
+  // Single carousel control button
+  var carouselButton = $("#carouselButton");
+  var carouselIcon = $("#carousel-button-icon");
+
+  // Initialize the carousel
+  $("#myCarousel").carousel();
+
+  // Handle carousel button click
+  carouselButton.click(function () {
+      if (carouselIcon.hasClass('fa-pause')) {
+          // Carousel is currently cycling, pause it
+          $("#myCarousel").carousel('pause');
+          carouselIcon.removeClass('fa-pause');
+          carouselIcon.addClass('fa-play');
+      } else {
+          // Carousel is currently paused, start cycling
+          $("#myCarousel").carousel('cycle');
+          carouselIcon.removeClass('fa-play');
+          carouselIcon.addClass('fa-pause');
+      }
+  });
 });
